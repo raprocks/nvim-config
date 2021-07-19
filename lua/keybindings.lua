9779ac0475849975d87f2aafb7d4c97ef3a9c3f8
Rohit Patil <rahulhimesh09@gmail.com>
2021-07-20 00:29:58 +0530

    [UPDATE] Add keymaps
    ported `Ctrl+w` to `:wincmd` and resize commands changes
    added Completion Menu keymaps

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -8,16 +8,16 @@ end
 mapper("n", "<Leader>qa", ":qa", {})
 
 -- Window Navigation Remaps; leader bound; prefix 'w' then direction
-mapper("n", "<Leader>wj", "<C-w>j", { noremap = true})
-mapper("n", "<Leader>wk", "<C-w>k", { noremap = true})
-mapper("n", "<Leader>wl", "<C-w>l", { noremap = true})
-mapper("n", "<Leader>wh", "<C-w>h", { noremap = true})
+mapper("n", "<Leader>wj", ":wincmd j<CR>", { noremap = true, silent=true})
+mapper("n", "<Leader>wk", ":wincmd k<CR>", { noremap = true, silent=true})
+mapper("n", "<Leader>wl", ":wincmd l<CR>", { noremap = true, silent=true})
+mapper("n", "<Leader>wh", ":wincmd h<CR>", { noremap = true, silent=true})
 
 -- Resizing Windows (M is alt, C is Ctrl)
-mapper("n", "<M-C-Up>", "<C-w>2-",{ noremap = true})
-mapper("n", "<M-C-Down>", "<C-w>2+",{ noremap = true})
-mapper("n", "<M-C-Left>", "<C-w>2<",{ noremap = true})
-mapper("n", "<M-C-Right>", "<C-w>2>",{ noremap = true})
+mapper("n", "<M-C-Up>", ":resize -2<CR>",{ noremap = true})
+mapper("n", "<M-C-Down>", ":resize +2<CR>",{ noremap = true})
+mapper("n", "<M-C-Left>", ":vertical resize -2<CR>",{ noremap = true})
+mapper("n", "<M-C-Right>", ":vertical resize +2<CR>",{ noremap = true})
 
 -- NvimTree; leader bound; prefix t; #maychange
 mapper("n", "<Leader>tt", ":NvimTreeToggle<CR>",{})
@@ -27,7 +27,28 @@ mapper("n", "<Leader>tf", ":NvimTreeFindFile<CR>",{})
 
 -- nvim-compe; direct
 mapper("i", "<C-Space>", "compe#complete()", { silent=true, noremap = true, expr = true})
-mapper("i", "<CR>", "compe#confirm('<CR>')", { silent=true, noremap = true, expr = true})
 mapper("i", "<C-e>", "compe#close('<C-e>')", { silent=true, noremap = true, expr = true})
 mapper("i", "<C-f>", "compe#scroll({ 'delta': +4})", { silent=true, noremap = true, expr = true})
 mapper("i", "<C-d>", "compe#scroll({ 'delta': -4})", { silent=true, noremap = true, expr = true})
+
+
+-- Fugitive/Git; leader bound; prefix g
+mapper("n", "<Leader>g", ":G<CR>", {silent=true, noremap=true})
+
+
+-- Completion menu
+local function escape_termcode(str)
+    return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+function _G.smart_tab()
+    return vim.fn.pumvisible() == 1 and escape_termcode'<C-n>' or escape_termcode'<Tab>'
+end
+
+mapper('i', "<Tab>", "v:lua.smart_tab()", {expr=true, noremap=true})
+
+function _G.smart_shift_tab()
+    return vim.fn.pumvisible() == 1 and escape_termcode'<C-p>' or escape_termcode'<S-Tab>'
+end
+
+mapper('i', "<S-Tab>", "v:lua.smart_shift_tab()", {expr=true, noremap=true})
