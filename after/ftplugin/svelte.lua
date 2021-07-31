4ef4610037d20aec5d90a6fc65981116bfa37b26
Rohit Patil <rahulhimesh09@gmail.com>
2021-08-01 00:31:24 +0530

    [UPDATE] disable emmet for html in svelte

--- after/ftplugin/svelte.lua
+++ after/ftplugin/svelte.lua
@@ -1,3 +1,17 @@
-require'lspconfig'.svelte.setup{}
-
+local capabilities_svelte = vim.lsp.protocol.make_client_capabilities()
+capabilities_svelte.textDocument.completion.completionItem.snippetSupport = true
+require'lspconfig'.svelte.setup{
+    capabilities = capabilities_svelte,
+    settings = {
+        svelte = {
+            plugin = {
+                html = {
+                    completions = {
+                        emmet = false
+                    }
+                }
+            }
+        }
+    }
+}
 require'lspconfig'['svelte'].manager.try_add_wrapper()
